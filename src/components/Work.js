2215d70b5e212643bc554dc32ea379f9517412c7
s594475 <543802872@qq.com>
2016-08-18 17:06:53 +0800

    wan

--- src/components/Work.js
+++ src/components/Work.js
@@ -9,32 +9,32 @@ class Word extends React.Component {
     }
         return(
           <div className='work-container'>
-  
-         <Card className="work-card">
+
+         <Card className="work-card work-list">
            <CardMedia overlay={<CardTitle title="百日百页面" subtitle="初学前端每日笔记" style={styles.title}/>} >
              <img src="http://7xopqp.com1.z0.glb.clouddn.com/contain1.jpg" />
            </CardMedia>
          </Card>
 
-         <Card className="work-card">
+         <Card className="work-card work-list">
            <CardMedia overlay={<CardTitle title="文档手册" subtitle="我的个人博客" style={styles.title}/>} >
              <img src="http://7xopqp.com1.z0.glb.clouddn.com/contain2.jpg" />
            </CardMedia>
          </Card>
 
-         <Card className="work-card">
+         <Card className="work-card work-list">
            <CardMedia overlay={<CardTitle title="自制时钟" subtitle="canvas 小 demo" style={styles.title}/>} >
              <img src="http://7xopqp.com1.z0.glb.clouddn.com/contain3.jpg" />
            </CardMedia>
          </Card>
 
-         <Card className="work-card">
+         <Card className="work-card work-list">
            <CardMedia overlay={<CardTitle title="其他案例" subtitle="一些练手作品" style={styles.title}/>} >
              <img src="http://7xopqp.com1.z0.glb.clouddn.com/contain-none.jpg" />
            </CardMedia>
          </Card>
 
-         <Card className="work-card">
+         <Card className="work-card work-list">
            <CardMedia overlay={<CardTitle title="初学小做" subtitle="大三上学期参赛作品" style={styles.title}/>} >
              <img src="http://7xopqp.com1.z0.glb.clouddn.com/contain4.jpg" />
            </CardMedia>
